a77855fff3ec83b624ed76130fb0ecce7a460fb2
clickjonel <139108979+clickjonel@users.noreply.github.com>
2025-09-03 16:38:38 +0000

    HRH User

---
+++ pkp_api/database/migrations/2025_09_03_160101_create_hrhs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('pkpulse')->create('hrh_user', function (Blueprint $table) {
+            $table->id('hrh_user_id');
+            $table->string('user_code');
+            $table->string('image');
+            $table->string('username');
+            $table->string('password');
+            $table->string('prefix')->nullable();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('suffix')->nullable();
+            $table->string('nickname');
+            $table->string('email_address');
+            $table->string('contact_number');
+            $table->string('account_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('pkpulse')->dropIfExists('hrhs');
+    }
+};
