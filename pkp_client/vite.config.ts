b66544f5d63e7989ee3c61f3c23d6c694f83318f
Samuel Corpuz <xdsamuelcorpuz@gmail.com>
2025-09-04 22:56:11 +0800

    remove the vite-plugin-javascript-obfuscator

--- pkp_client/vite.config.ts
+++ pkp_client/vite.config.ts
@@ -2,46 +2,11 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
 
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
-    obfuscatorPlugin({
-      exclude: [/node_modules/],
-      apply: 'build',
-      options: {
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        debugProtection: false,
-        debugProtectionInterval: 0,
-        disableConsoleOutput: true,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: true,
-        renameGlobals: false,
-        selfDefending: false,
-        simplify: true,
-        splitStrings: true,
-        splitStringsChunkLength: 10,
-        stringArray: true,
-        stringArrayEncoding: ['base64'],
-        stringArrayIndexShift: true,
-        stringArrayRotate: true,
-        stringArrayShuffle: true,
-        stringArrayWrappersCount: 2,
-        stringArrayWrappersChainedCalls: true,
-        stringArrayWrappersParametersMaxCount: 4,
-        stringArrayWrappersType: 'function',
-        stringArrayThreshold: 0.75,
-        transformObjectKeys: true,
-        unicodeEscapeSequence: false,
-      },
-    }),
+    tailwindcss()
   ],
   resolve: {
     alias: {
