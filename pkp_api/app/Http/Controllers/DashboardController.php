287ab0352f6d92b2419f42e3fe7b0ef37da76a6d
clickjonel <139108979+clickjonel@users.noreply.github.com>
2025-09-11 13:42:08 +0000

    Dashboard

---
+++ pkp_api/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hrh;
+use App\Models\Pkp_indicator;
+use App\Models\Pkp_site;
+use App\Models\Programs;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function getExecDashboardData():JsonResponse
+    {
+        $programs_count = Programs::count();
+        $indicators_count = Pkp_indicator::count();
+        $pk_sites_count = Pkp_site::count();
+        $hrh_count = Hrh::where('user_level',5)->count();
+        $indicators = Pkp_indicator::with(['program','disaggregations'])->get();
+
+        return response()->json([
+            'data' => [
+                'programs_count' => $programs_count,
+                'indicators_count' => $indicators_count,
+                'pk_sites_count' => $pk_sites_count,
+                'hrh_count' => $hrh_count,
+                'indicators' => $indicators,
+            ]
+        ]);
+    }
+}
--- pkp_client/src/pages/admin/dashboard.vue
+++ pkp_client/src/pages/admin/dashboard.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { onMounted, ref } from 'vue';
     import { Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle } from "@/components/ui/card";
     import { HousePlus,TriangleAlert  } from 'lucide-vue-next';
-    import { Table,TableBody,TableCell,TableHead,TableHeader,TableRow } from '@/components/ui/table'
+    import { Table,TableBody,TableCell,TableHead,TableHeader,TableRow } from '@/components/ui/table';
+    import axios from '@/axios/axios';
 
-    function test(){
-        console.log('asdasd')
+    const dashboardData = ref({
+        programs_count:0,
+        indicators:{},
+        indicators_count:0
+    })
+
+    onMounted(()=>{
+        getData()
+    })
+
+    function getData(){
+        axios.get('/exec/dashboard',{})
+        .then((response)=>{
+            console.log(response.data.data)
+            dashboardData.value = response.data.data
+        })
     }
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col justify-start items-start gap-2 p-2">
+    <div class="w-full h-full flex flex-col justify-start items-start gap-2 p-2 overflow-y-scroll">
        
         <div class="w-full grid grid-cols-4 gap-4 p-2">
             <Card class="w-full">
@@ -24,7 +39,7 @@
                     <CardDescription>Current total of PK Programs</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Content
+                    <span class="text-5xl font-black">{{ dashboardData.programs_count }}</span>
                 </CardContent>
             </Card>
 
@@ -39,7 +54,7 @@
                     <CardDescription>Total number of Indicators for PK</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Content
+                    <span class="text-5xl font-black">{{ dashboardData.indicators_count }}</span>
                 </CardContent>
             </Card>
 
@@ -54,7 +69,7 @@
                     <CardDescription>Total PK Sites</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Content
+                    <span class="text-5xl font-black">{{ dashboardData.pk_sites_count }}</span>
                 </CardContent>
             </Card>
 
@@ -69,61 +84,49 @@
                     <CardDescription>Total HRH</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    Content
+                    <span class="text-5xl font-black">{{ dashboardData.hrh_count }}</span>
                 </CardContent>
             </Card>
         </div>
 
-        <div class="w-full flex flex-col justify-start items-start">
-            <span class="text-lg uppercase font-semibold p-2">Program Indicators</span>
-            <Table>
+        <div class="w-full h-[570px] flex flex-col justify-start items-start">
+            <span class="text-lg uppercase font-semibold p-2 ml-3">Program Indicators</span>
+            <!-- <Table>
                 <TableHeader>
                     <TableRow>
-                        <TableHead>Indicator</TableHead>
                         <TableHead>Program</TableHead>
+                        <TableHead>Indicator</TableHead>
                         <TableHead>Scope</TableHead>
                         <TableHead>Disaggregation</TableHead>
                         <TableHead>Status</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <!-- <TableRow v-for="hrh in hrhList">
-                        <TableCell>{{ hrh.first_name }}</TableCell>
-                        <TableCell>{{ hrh.middle_name }}</TableCell>
-                        <TableCell>{{ hrh.last_name }}</TableCell>
-                        <TableCell>{{ hrh.user_level_name }}</TableCell>
-                        <TableCell class="w-full flex justify-end items-center gap-2">
-                            <Popover>
-                                <PopoverTrigger asChild>
-                                    <Button variant="ghost" size="icon" class="cursor-pointer">
-                                        <EllipsisVertical class="h-4 w-4" />
-                                    </Button>
-                                </PopoverTrigger>
-                                <PopoverContent class="w-45 p-2">
-                                    <div class="flex flex-col">
-                                        <Button @click="openAssignHrhModal(hrh)" variant="ghost" size="sm" class="justify-start text-xs">
-                                            <UserRoundCog/> 
-                                            Assign Team
-                                        </Button>
-                                        <Button @click="openAssignHrhModal(hrh)" variant="ghost" size="sm" class="justify-start text-xs">
-                                            <UserRoundCog/> 
-                                            Update Details
-                                        </Button>
-                                            <Button @click="openAssignHrhModal(hrh)" variant="ghost" size="sm" class="justify-start text-xs">
-                                            <UserRoundCog/> 
-                                            Update Credentials
-                                        </Button>
-                                        <Button @click="confirmDeactivateHrh(hrh.hrh_user_id)" variant="ghost" size="sm" class="justify-start text-xs text-red-600">
-                                            <UserLock /> 
-                                            Deactivate
-                                        </Button>
-                                    </div>
-                                </PopoverContent>
-                            </Popover>
-                        </TableCell>
-                    </TableRow> -->
+                    <TableRow v-for="indicator in dashboardData.indicators">
+                        <TableCell>{{ indicator.program.program_name }}</TableCell>
+                        <TableCell>{{ indicator.indicator_name }}</TableCell>
+                        <TableCell>{{ indicator.indicator_scope }}</TableCell>
+                        <TableCell>{{ indicator.disaggregation }}</TableCell>
+                        <TableCell>{{ indicator.indicator_status }}</TableCell>
+                    </TableRow>
                 </TableBody>
-            </Table>
+            </Table> -->
+            <div class="w-full flex flex-col justify-start items-start p-4">
+                <div class="w-full flex justify-start items-stretch divide-x border-t border-x divide-black border-black text-center uppercase font-semibold">
+                    <span class="w-[15%] p-2">Program</span>
+                    <span class="w-[40%] p-2">Indicator</span>
+                    <span class="w-[15%] p-2">Scope</span>
+                    <span class="w-[15%] p-2">Disaggregation</span>
+                    <span class="w-[15%] p-2">Status</span>
+                </div>
+                <div v-for="indicator in dashboardData.indicators" class="w-full flex justify-start items-stretch divide-x border-t border-x divide-black border-black text-left text-sm font-light last:border-b">
+                    <span class="p-2 w-[15%]">{{ indicator.program.program_name }}</span>
+                    <span class="p-2 w-[40%]">{{ indicator.indicator_name }}</span>
+                    <span class="p-2 w-[15%]">{{ indicator.indicator_scope === 1 ? 'Individual Scope' : 'Household Scope' }}</span>
+                    <span class="p-2 w-[15%]">{{ indicator.disaggregation }}</span>
+                    <span class="p-2 w-[15%]">{{ indicator.indicator_status === 1 ? 'Active' : 'Deactivated' }}</span>
+                </div>
+            </div>
         </div>
 
     </div>
