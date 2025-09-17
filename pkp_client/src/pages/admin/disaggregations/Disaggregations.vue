33540a82b222cc8517ce193197040acf4802d2ed
ramlittle <ramelitomartinez@gmail.com>
2025-09-18 01:08:37 +0800

    FE-feat: made edit feature for disaggregations work

--- pkp_client/src/pages/admin/disaggregations/Disaggregations.vue
+++ pkp_client/src/pages/admin/disaggregations/Disaggregations.vue
@@ -35,6 +35,12 @@ const currentList = ref<Disaggregation[]>([]);
 let generatedCode = ref("");
 let searchKeyword = ref("");
 let isCreateModalOpen = ref(false);
+let isEditModalOpen = ref(false);
+let recordToEdit = ref<Disaggregation>({
+    disaggregation_id: 0,
+    disaggregation_code: "",
+    disaggregation_name: "",
+});
 
 function search() {
     const searchTerm = searchKeyword.value.toLowerCase();
@@ -121,6 +127,44 @@ function handleClose() {
     isCreateModalOpen.value = false;
 }
 
+function handleEdit(record: Disaggregation) {    
+    recordToEdit.value = { ...record };
+    isEditModalOpen.value = true;
+}
+
+function confirmEdit() {
+    axios
+        .put("/disaggregation/update", {
+            disaggregation_id: recordToEdit.value.disaggregation_id,
+            disaggregation_code: recordToEdit.value.disaggregation_code,
+            disaggregation_name: recordToEdit.value.disaggregation_name
+        })
+        .then((response) => {
+            console.log(response.data);
+            toast("record updated successfully!", {
+                description: response.data.message,
+                action: {
+                    label: "Close",
+                    onClick: () => toast.dismiss(),
+                },
+            });
+            fetchList();
+            isEditModalOpen.value = false;
+        })
+        .catch((error) => {
+            console.error(error.response.data);
+            if (error.response) {
+                toast("Failed to update program", {
+                    description: error.response.data.message,
+                    action: {
+                        label: "Close",
+                        onClick: () => toast.dismiss(),
+                    },
+                });
+            }
+        });
+}
+
 onMounted(() => {
     fetchList();
 });
@@ -169,7 +213,12 @@ onMounted(() => {
                                 </PopoverTrigger>
                                 <PopoverContent>
                                     <div class="flex flex-col gap-1">
-                                        <span>buttons here</span>
+                                        <Button variant="ghost" size="sm"
+                                            class="cursor-pointer text-xs flex justify-start items-center gap-1"
+                                            @click="handleEdit(record)">
+                                            <Pencil class="h-[1rem] w-[1rem]" />
+                                            Edit
+                                        </Button>
                                     </div>
                                 </PopoverContent>
                             </Popover>
@@ -206,6 +255,35 @@ onMounted(() => {
                 <Button type="submit" class="cursor-pointer hover:bg-red-500" @click="handleClose()">Cancel</Button>
                 <Button type="submit" class="cursor-pointer hover:bg-emerald-500 hover:text-black"
                     @click="handleCreate">Submit</Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+
+    <Dialog v-model:open="isEditModalOpen">
+        <DialogContent class="font-poppins w-[20rem] md:max-w-[20rem] lg:max-w-[50rem]">
+            <DialogHeader>
+                <DialogTitle>Edit</DialogTitle>
+                <DialogDescription>Apply edit here</DialogDescription>
+            </DialogHeader>
+            <div class="w-full flex flex-col justify-start items-start gap-2 p-2 border">
+                <form @submit.prevent="confirmEdit" class="flex flex-col gap-4">
+                    <div class="flex flex-col gap-2">
+                        <label for="disaggregation_code">Program Code:</label>
+                        <Input type="text" class="bg-slate-200" id="disaggregation_code" placeholder="NP-0001"
+                            v-model="recordToEdit.disaggregation_code" readonly />
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="disaggregation_name">Disaggregation Name:</label>
+                        <Input type="text" id="disaggregation_name" placeholder="Enter here..."
+                            v-model="recordToEdit.disaggregation_name" required />
+                    </div>
+                    <!-- Add more fields as needed -->
+                </form>
+            </div>
+            <DialogFooter>
+                <Button type="button" class="cursor-pointer" @click="isEditModalOpen = false">Cancel</Button>
+                <Button type="button" class="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white"
+                    @click="confirmEdit">Save</Button>
             </DialogFooter>
         </DialogContent>
     </Dialog>
