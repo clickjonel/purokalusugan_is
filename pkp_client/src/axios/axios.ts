945270179215a3d1a2f6caa0ac3567af6a28d9ae
portald3v <134351375+portald3v@users.noreply.github.com>
2025-09-29 07:37:41 +0800

    changes on the integration of backend and frontnend

--- pkp_client/src/axios/axios.ts
+++ pkp_client/src/axios/axios.ts
@@ -2,7 +2,7 @@ import axiosInstance from "axios";
 
 const instance = axiosInstance.create({
     //   baseURL: import.meta.env.VITE_BACKEND_API,
-    baseURL: 'http://192.168.225.100:8000/api/',
+    baseURL: 'http://210.213.80.10:9500/api/',
     timeout: 10000,
     headers: {
         'Accept': 'application/json',
--- pkp_client/vite.config.ts
+++ pkp_client/vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server:{
+    host: '0.0.0.0',
+    port: 9002,
+  }
   // build: {
   //   // outDir: path.resolve(__dirname, '../pkp_api/public/pkp/'), // Laravel's public folder
   //   emptyOutDir: true,
